ab9676394452b454e62441cb6cc01837403e406d
Deyfran Aragón <137656348+Deyfran58@users.noreply.github.com>
2025-11-08 11:28:21 -0600

    Se creo la encuentra junto con sus datos correctamente (#23)

--- app/src/main/java/cr/ac/utn/practicaltest/MainActivity.kt
+++ app/src/main/java/cr/ac/utn/practicaltest/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -52,94 +53,94 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        val btnSurvey = findViewById<Button>(R.id.btnSurvery_main)
-        btnSurvey.setOnClickListener(View.OnClickListener{ view->
-            
-        })
+        val btnSurvey = findViewById<Button>(R.id.btnSurvey_main)
+        btnSurvey.setOnClickListener {
+            Util.openActivity(this, SurveyActivity::class.java)
+        }
 
         val btnReceipt = findViewById<Button>(R.id.btnReceipt_main)
         btnReceipt.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnService = findViewById<Button>(R.id.btnService_main)
         btnService.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnSupplier = findViewById<Button>(R.id.btnSupplier_main)
         btnSupplier.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnIncident = findViewById<Button>(R.id.btnIncident_main)
         btnIncident.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnVehicle = findViewById<Button>(R.id.btnVehicle_main)
         btnVehicle.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnVisit = findViewById<Button>(R.id.btnVisit_main)
         btnVisit.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnMeeting = findViewById<Button>(R.id.btnMeeting_main)
         btnMeeting.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnTraining = findViewById<Button>(R.id.btnTraining_main)
         btnTraining.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnInventory = findViewById<Button>(R.id.btnInventory_main)
         btnInventory.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnVolunteer = findViewById<Button>(R.id.btnVolunteer_main)
         btnVolunteer.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnEntertainment = findViewById<Button>(R.id.btnEntertainment_main)
         btnEntertainment.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnLibrary = findViewById<Button>(R.id.btnLibrary_main)
         btnLibrary.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnRent = findViewById<Button>(R.id.btnRent_main)
         btnRent.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnLearning = findViewById<Button>(R.id.btnLearning_main)
         btnLearning.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnHarvest = findViewById<Button>(R.id.btnHarvest_main)
         btnHarvest.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnMeal = findViewById<Button>(R.id.btnMeal_main)
         btnMeal.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnSponsor = findViewById<Button>(R.id.btnSponsor_main)
         btnSponsor.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
 
         val btnFilm = findViewById<Button>(R.id.btnFilm_main)
@@ -149,7 +150,7 @@ class MainActivity : AppCompatActivity() {
 
         val btnRecipe = findViewById<Button>(R.id.btnRecipe_main)
         btnRecipe.setOnClickListener(View.OnClickListener{ view->
-            
+
         })
     }
 }
